eba23cb34fab88a9d802e0c5a7071e52511fb2aa
Diego <dapm@jupiter.local>
2023-08-21 19:59:51 -0400

    Agregar archivos RowMapper a model

--- src/main/java/com/example/demo/dao/UsuariosDAOImpl.java
+++ src/main/java/com/example/demo/dao/UsuariosDAOImpl.java
@@ -1,5 +1,15 @@
 package com.example.demo.dao;
 
-public class UsuariosDAOImpl {
+import java.util.List;
+
+import com.example.demo.model.Usuarios;
+
+public class UsuariosDAOImpl implements UsuariosDAO {
+
+	@Override
+	public List<Usuarios> ListarUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
