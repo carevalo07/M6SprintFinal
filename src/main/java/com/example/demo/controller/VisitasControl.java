95a61b67d51e7196c2c9dced5cee08a635552f7c
Diego <dapm@jupiter.local>
2023-08-21 21:24:47 -0400

    agregar visitas control dao daoimpl

--- src/main/java/com/example/demo/controller/VisitasControl.java
+++ src/main/java/com/example/demo/controller/VisitasControl.java
@@ -1,5 +1,30 @@
 package com.example.demo.controller;
 
-public class VisitasControl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Capacitaciones;
+
+@Controller
+@RequestMapping("/")
+public class VisitasControl {
+  private final VisitasDAO visitasDAO;
+ 
+  @Autowired  
+  public VisitasControl(VisitasDAO visitasDAO) {
+	  this.visitasDAO  = visitasDAO;
+  }
+  
+  @GetMapping("/listarVisitas")
+	public String listarVisitas(Model model) {
+	  List<Visitas> listarVisitas =  visitasDAO.ListarVisitas();
+	  model.addAttribute("listarVisitas", listarVisitas);
+    return "listarvisitas";
+	} 
+  
 }
